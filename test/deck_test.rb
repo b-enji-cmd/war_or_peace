823f1ddcfffa4cf7d3b5d07f154e5b21d4e72a37
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-01 16:32:48 -0700

    Add passing deck attribute test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
 class DeckTest < Minitest::Test
   def setup
@@ -14,5 +15,10 @@ class DeckTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Deck, @deck
+  end
+
+  def test_it_can_see_cards
+    cards = [@card1,@card2,@card3]
+    assert_equal cards , @deck.cards
   end
 end
