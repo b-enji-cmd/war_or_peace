460f29c9bda00f639f4891d8584a931f9af8135f
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-01 17:04:49 -0700

    Add passing test for high ranking cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,10 @@ class Deck
   def rank_of_card_at(arg_index)
     @cards.fetch(arg_index).rank
   end
+
+  def high_ranking_cards
+    @cards.find_all do |card|
+      card.rank >= 11
+    end
+  end
 end
