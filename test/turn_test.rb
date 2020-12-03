937ab8127bce14b867c260c01fd05bb83eeebbdb
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-03 15:44:53 -0700

    Update assertion statements

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -72,8 +72,8 @@ class TurnTest < Minitest::Test
     @turn.pile_cards
     @turn.spoils_of_war
     @turn.award_spoils(winner)
-    assert_equal [@card2,@card5,@card8,@card1,@card3], @player1.deck
-    assert_equal [@card4,@card6,@card7], @player2.deck
+    assert_equal [@card2,@card5,@card8,@card1,@card3], @player1.deck.cards
+    assert_equal [@card4,@card6,@card7], @player2.deck.cards
   end
 
 end
