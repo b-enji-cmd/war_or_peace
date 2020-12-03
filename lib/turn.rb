db437453a2f0ce5e81a28660619dcd79bc1da2af
b-enji-cmd <63377504+b-enji-cmd@users.noreply.github.com>
2020-12-03 11:48:39 -0700

    Add winner calculation UNFINISHED

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,12 +10,34 @@ class Turn
     end
 
     def type
-        if ((@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0))  &&  @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
-              :mutually_assured_destruction
-        elsif (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0))
-            :war
+       if ((@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0))  &&  @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
+        :mutually_assured_destruction
+       elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+        :war
+       else @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+        :basic
+       end
+    end
+
+    def calculate_winner(player1, player2, card_index)
+        if player1.deck.rank_of_card_at(card_index) < player2.deck.rank_of_card_at(card_index)
+            player2
         else
-            :basic
+            player1
+        end
+    end
+
+    def winner
+        victor = type
+        case victor
+        when :basic
+            calculate_winner(@player1,@player2, 0)
+        when :war
+            calculate_winner(@player1,@player2, 2)
+        when :mutually_assured_destruction
+            "No Winner"
+        else
+            "Something went wrong!"
         end
     end
 end
