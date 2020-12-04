d69e717826b4f6150fc48aa0828615a7d35b7858
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-03 17:14:32 -0700

    Add spoilage collection for war basic and mutual testing

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -15,6 +15,7 @@ class TurnTest < Minitest::Test
     @card6 = Card.new(:diamond, 'Queen', 12)
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
+    @testing_deck = [@card1,@card2,@card3,@card4,@card5,@card6,@card7,@card8]
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
     @deck2 = Deck.new([@card3, @card4, @card6, @card7])
     @player1 = Player.new("Megan", @deck1)
@@ -76,4 +77,29 @@ class TurnTest < Minitest::Test
     assert_equal [@card4,@card6,@card7], @player2.deck.cards
   end
 
+  def test_it_can_award_spoils_for_war
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal [@card8],player1.deck.cards
+    assert_equal [@card7, @card1,@card4,@card2,@card3,@card5,@card6], player2.deck.cards
+
+  end
+  def test_it_can_award_spoils_for_mutual
+    card6 = Card.new(:diamond, '8', 8)
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card4, @card3, card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    assert_equal "No Winner", winner
+    assert_equal [], turn.spoils_of_war
+  end
 end
